c553f1be6ddaf87d874f09969e7b9e7d898965f9
miha-9111 <miha-9111@yandex.ru>
2021-02-08 18:30:43 +0300

    checkSpam function

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -4,5 +4,11 @@
  * @returns {boolean}
  */
 function checkSpam(str) {
-  // ваш код...
+  let lowerStr = str.toLowerCase();
+
+  if (lowerStr.includes('1xbet') || lowerStr.includes('xxx')) {
+    return true;
+  } else {
+    return false;
+  }
 }
