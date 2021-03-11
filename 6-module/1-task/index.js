7608aa907b2484e0420a85709e0bf759cd6c596e
miha-9111 <miha-9111@yandex.ru>
2021-03-11 20:32:32 +0300

    module-6

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -14,5 +14,22 @@
  */
 export default class UserTable {
   constructor(rows) {
+    this.elem = document.createElement('table');
+
+    let tbodyTemp = '';
+
+    for (let objRow of rows) {
+      tbodyTemp += `<tr><td>${objRow.name}</td><td>${objRow.age}</td><td>${objRow.salary}</td><td>${objRow.city}</td><td><button>X</button></td></tr>`;
+    }
+
+    this.elem.insertAdjacentHTML('afterbegin', `<thead><tr><th>Имя</th><th>Возраст</th><th>Зарплата</th><th>Город</th><th></th></tr></thead><tbody>${tbodyTemp}</tbody>`);
+
+    this.elem.onclick = function(event) {
+      let target = event.target;
+
+      if (target.tagName === 'BUTTON') {
+        target.closest('tr').remove();
+      }
+    };
   }
 }
